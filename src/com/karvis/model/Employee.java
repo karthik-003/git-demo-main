d8e4b61a7171d0fddde88e7e411e72c78fb0e07d
Karthik <karthikdy@gmail.com>
2020-04-30 14:20:44 +0530

    Initial Commit

---
+++ src/com/karvis/model/Employee.java
@@ -0,0 +1,29 @@
+package com.karvis.model;
+
+public class Employee {
+
+	String name;
+	long empId;
+	String city;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public long getEmpId() {
+		return empId;
+	}
+	public void setEmpId(long empId) {
+		this.empId = empId;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	
+}
